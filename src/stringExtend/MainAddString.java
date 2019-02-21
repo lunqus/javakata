c3d5fd3ee0f68100725ebe7eb72ec490dec80d94
LUNKUS <sigitas@lunkus.com>
2019-02-21 10:56:47 +0200

    string extend

---
+++ src/stringExtend/MainAddString.java
@@ -0,0 +1,15 @@
+package stringExtend;
+
+public class MainAddString {
+
+    public static void main(String[] args) {
+
+        addStr addStr = new addStr();
+
+        System.out.println(addStr.add("Volare, Volare"));
+
+        StringBuilder gonon = new StringBuilder();
+        gonon.append()
+
+    }
+}
---
+++ src/stringExtend/addStr.java
@@ -0,0 +1,9 @@
+package stringExtend;
+
+public class addStr {
+
+    String add(String string) {
+        return string;
+    }
+
+}
